0d80359c8f451d6fff2abd7e07c7e32dc02ec06b
Vlad Zubin <zoobeen1@gmail.com>
2022-01-17 00:10:10 +0200

    header+hero+mob-menu -- final(mobil version)

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,11 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuLnk1 = document.querySelector('.link-1');
+  const closeMenuLnk2 = document.querySelector('.link-2');
+  const closeMenuLnk3 = document.querySelector('.link-3');
+  const closeMenuLnk4 = document.querySelector('.link-4');
+  const closeMenuLnk5 = document.querySelector('.link-5');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -14,6 +19,11 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuLnk1.addEventListener('click', toggleMenu);
+  closeMenuLnk2.addEventListener('click', toggleMenu);
+  closeMenuLnk3.addEventListener('click', toggleMenu);
+  closeMenuLnk4.addEventListener('click', toggleMenu);
+  closeMenuLnk5.addEventListener('click', toggleMenu);
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
