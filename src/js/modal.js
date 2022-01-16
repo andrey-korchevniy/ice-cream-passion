b26dafe543b76c2c8dc8059fe45c94acae7fabaa
Fefaodessa <fefaodessa@gmail.com>
2022-01-16 17:34:40 +0200

    final 480px

--- src/js/modal.js
+++ src/js/modal.js
@@ -9,6 +9,22 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('target');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('');
+  }
+})();
+
