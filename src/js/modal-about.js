e099a1432ee676acc1f85c118a13f588a76f3bbb
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-01-17 02:04:00 +0200

    add btn's to modal and tablet ver.
    too RAW version)

--- src/js/modal-about.js
+++ src/js/modal-about.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('.about-modal-open'),
+    closeModalBtn: document.querySelector('.about-modal-close'),
+    modal: document.querySelector('.about-modal-connector'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('is-hidden-about');
   }
 })();
