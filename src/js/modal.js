76b63f607b852d006aa91058626c3a4ab9fa5279
Fefaodessa <fefaodessa@gmail.com>
2022-01-18 16:28:58 +0200

    JS

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,30 @@
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+var modal = document.getElementById('modal-location');
+var btn = document.getElementById("btn-modal-location");
+var video = document.getElementById("video");
+var span = document.getElementsByClassName("modal-location-close")[0];
+btn.onclick = function() {
+ video.play()
+    modal.style.display = "block";
+    let start = Date.now();
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  let timer = setInterval(function() {
+    let timePassed = Date.now() - start;
 
-  function toggleModal() {
-    refs.modal.classList.toggle('target');
-  }
-})();
+    video.style.display = "none";
+
+    if (timePassed > 9000) clearInterval(timer);
+
+  }, 6000);
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
@@ -27,17 +39,4 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
-
-video.onclick = function() {
-  let start = Date.now();
-
-  let timer = setInterval(function() {
-    let timePassed = Date.now() - start;
-
-    video.style.left = timePassed / 3 + 'px';
-
-    if (timePassed > 9000) clearInterval(timer);
-
-  }, 6000);
 }
